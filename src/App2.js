6704d1268e2504d3e7c4e3f9fac0869e364939bb
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-07-20 19:35:07 +0500

    start & stop

--- src/App2.js
+++ src/App2.js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
 
 class App2 extends Component {
+  state = {
+    second: 0,
+    minute: 0,
+    hour: 0,
+    btnDisabled: false,
+    interval:""
+  };
+
+  startClicked = () => {
+    this.setState({
+      btnDisabled:true
+    })
+   let timer = setInterval(() => {
+      const { second, minute, hour } = this.state;
+      if (second === 59) {
+        if (minute === 59) {
+          this.setState({
+            second: 0,
+            minute: 0,
+            hour: hour + 1,
+          });
+        } else {
+          this.setState({
+            second: 0,
+            minute: minute + 1,
+          });
+        }
+      } else {
+        this.setState({
+          second: second + 1,
+        });
+      }
+    }, 1000);
+    this.setState({
+      interval:timer
+    })
+  };
+
+  stopClicked = () => {
+    clearInterval(this.state.interval);
+    this.setState({
+      btnDisabled:false
+    })
+  };
+
   render() {
+    const { second, minute, hour, btnDisabled } = this.state;
     return (
       <div>
         <div className="timer-container ">
@@ -9,25 +55,29 @@ class App2 extends Component {
             <span>Online</span> StopWatch
           </h1>
           <div className="timer-col">
-            <p className="timer-hours">0</p>
+            <p className="timer-hours">{hour}</p>
             <p className="timer-label">Hours</p>
           </div>
 
           <div className="timer-col">
-            <p className="timer-minutes">0</p>
+            <p className="timer-minutes">{minute}</p>
             <p className="timer-label">Minutes</p>
           </div>
           <div className="timer-col">
-            <p className="timer-seconds">0</p>
+            <p className="timer-seconds">{second}</p>
             <p className="timer-label">Seconds</p>
           </div>
         </div>
         <div className="timer-container text-center">
           <div className="timerBtn">
-            <button className="btn  btn-success">Start</button>
+            <button className="btn  btn-success" onClick={this.startClicked}>
+              Start
+            </button>
           </div>
           <div className="timerBtn">
-            <button className="btn  btn-danger">Stop</button>
+            <button className="btn  btn-danger" onClick={this.stopClicked}>
+              Stop
+            </button>
           </div>
           <div className="timerBtn">
             <button className="btn  btn-secondary">Interval</button>
